da3d93e5a7c864b6e3540ff54ff585b28580fbb7
Perepelov Alexander <asperepelov@mail.ru>
2024-04-23 21:34:16 +0300

    Задание 4

---
+++ Lesson10/Task4/go.mod
@@ -0,0 +1,6 @@
+module task4
+
+go 1.22.0
+
+require github.com/asperepelov/goHello/v2 v2.0.1
+require github.com/asperepelov/goHello v1.0.4
---
+++ Lesson10/Task4/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/asperepelov/goHello"
+	v2 "github.com/asperepelov/goHello/v2"
+)
+
+func main() {
+	goHello.Hello()
+	v2.Hello()
+}
